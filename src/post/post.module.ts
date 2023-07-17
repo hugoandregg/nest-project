bef7bcb5f6b1688c19dd434dc2ccbf1882c14e0d
Hugo Galvão <hugo.galvao@klarna.com>
2023-07-17 17:14:35 +0200

    Require auth for post endpoints

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 import { PrismaService } from 'src/services/prisma.service';
+import { requireAuth } from 'src/middleware/auth.middleware';
 
 @Module({
   controllers: [PostController],
   providers: [PostService, PrismaService],
 })
-export class PostModule {}
+export class PostModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(requireAuth) // Apply the requireAuth middleware
+      .forRoutes('/posts'); // Apply the middleware to all routes in the module
+  }
+}
